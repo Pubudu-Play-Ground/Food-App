5ea44f265ad918146c837e5905cb5f33fc5bfe00
Pubudu Ariyawansha <pubuduariyawansa@gmail.com>
2023-09-25 18:11:44 +0530

    update1

---
+++ MyFoodAPp/src/main/java/lk/ijse/dep11/tm/Customer.java
@@ -0,0 +1,19 @@
+package lk.ijse.dep11.tm;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+public class Customer {
+    String id;
+    String name;
+    String contact;
+    String fooditem;
+    int qty;
+
+}
